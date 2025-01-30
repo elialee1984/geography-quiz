02418b6d221e038a309439be51c27b07a058317c
Elia Lee <elia.lee1984@gmail.com>
2025-01-29 22:04:04 -0500

    created CountryNames Component

---
+++ geographyQuiz/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CountryNames from "./components/countryNames";
+
+function App() {
+  return (
+    <div>
+      <CountryNames />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ geographyQuiz/src/components/CountryNames.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CountryNames = () => {
+  return <div>Das ist ein Test, mal sehen, ob es klappt!</div>;
+};
+
+export default CountryNames;
