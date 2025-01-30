a63345576959aa03209861373d922982b200ad58
Elia Lee <elia.lee1984@gmail.com>
2025-01-29 23:04:32 -0500

    rough version of list with country names, official names, capitals, languages, demonyms, and currencies

--- geographyQuiz/src/App.jsx
+++ geographyQuiz/src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
 import CountryNames from "./components/countryNames";
 
-function App() {
+const App = () => {
   return (
     <div>
       <CountryNames />
     </div>
   );
-}
+};
 
 export default App;
-
