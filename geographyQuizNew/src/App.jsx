d1b3e09f9350ef28d27c8f6be00de364ba6d330c
Elia Lee <elia.lee1984@gmail.com>
2025-02-06 14:02:49 -0500

    Added React Router

--- geographyQuizNew/src/App.jsx
+++ geographyQuizNew/src/App.jsx
@@ -1,9 +1,14 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import "./App.css";
 import CountryNames from "./components/CountryNames";
+
+const router = createBrowserRouter();
 
 const App = () => {
   return (
     <div>
+      <RouterProvider router={router} />
       <CountryNames />
     </div>
   );
